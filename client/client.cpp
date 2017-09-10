fccb71197036f631ae58dd77f43cfe7ba5e6ee36
pipo07pipo <a5720110a@gmail.com>
2017-09-11 00:09:01 +0700

    Update client.cpp

--- client/client.cpp
+++ client/client.cpp
@@ -2,17 +2,26 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <stdio.h>
- 
- 
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <streambuf>
+#include "md5.h"
+#include <openssl/md5.h>
+using namespace std;
+
+
+
 int fileSEND(char *server, int PORT, char *lfile, char *rfile){
     int socketDESC;
     struct sockaddr_in serverADDRESS;
     struct hostent *hostINFO;
-    FILE * file_to_send;
-    int ch;
+    //FILE * file_to_send;
+    char ch;
     char toSEND[1];
     char remoteFILE[4096];
-    int count1=1,count2=1, percent;
+    int nextPercent=1,currentPercent=1, percent;
  
  
     hostINFO = gethostbyname(server);
@@ -28,46 +37,54 @@ int fileSEND(char *server, int PORT, char *lfile, char *rfile){
     }
  
     serverADDRESS.sin_family = hostINFO->h_addrtype;
-    memcpy((char *) &serverADDRESS.sin_addr.s_addr,    hostINFO->h_addr_list[0], hostINFO->h_length);
+    memcpy((char *) &serverADDRESS.sin_addr.s_addr, hostINFO->h_addr_list[0], hostINFO->h_length);
     serverADDRESS.sin_port = htons(PORT);
                  
     if (connect(socketDESC, (struct sockaddr *) &serverADDRESS, sizeof(serverADDRESS)) < 0) {
         printf("Cannot connect\n");
         return 1;
     }
- 
- 
-    file_to_send = fopen (lfile,"r");
+    std::fstream file_to_send;
+    file_to_send.open (lfile, std::fstream::in | std::fstream::out | std::fstream::app);
     if(!file_to_send) {
         printf("Error opening file\n");
-        //close(socketDESC);
+        close(socketDESC);
         return 0;
         } else {
     long fileSIZE;
-    fseek (file_to_send, 0, SEEK_END);     fileSIZE =ftell (file_to_send);
-    rewind(file_to_send);
- 
+    std::string str;
+    file_to_send.seekg(0,std::ios::end);
+    str.reserve(file_to_send.tellg());
+    str.assign( (std::istreambuf_iterator<char>(file_to_send) ),
+                        (std::istreambuf_iterator<char>()    )  );
+    
+
+    fileSIZE = file_to_send.tellg();
+    file_to_send.seekg(0,std::ios::beg);
+    
+    
     sprintf(remoteFILE,"FBEGIN:%s:%d\r\n", rfile, fileSIZE);
     send(socketDESC, remoteFILE, sizeof(remoteFILE), 0);
  
     percent = fileSIZE / 100;
-    while((ch=getc(file_to_send))!=EOF){
+    while(file_to_send.get(ch)){
         toSEND[0] = ch;
         send(socketDESC, toSEND, 1, 0);
-        if( count1 == count2 ) {
+        if( nextPercent == currentPercent ) {
             printf("33[0;0H");
             printf( "\33[2J");
             printf("Filename: %s\n", lfile);
             printf("Filesize: %d Kb\n", fileSIZE / 1024);
-            printf("Percent : %d%% ( %d Kb)\n",count1 / percent ,count1 / 1024);
-            count1+=percent;
+            printf("Percent : %d%% ( %d Kb)\n",nextPercent / percent ,nextPercent / 1024);
+            nextPercent+=percent;
         }
-        count2++;
+        currentPercent++;
  
     }
+    cout << "md5 of this file : " << md5(str) << endl;
     }
-    fclose(file_to_send);
-    //close(socketDESC);
+    file_to_send.close();
+    close(socketDESC);
  
     return 0;
 }
@@ -75,6 +92,6 @@ int fileSEND(char *server, int PORT, char *lfile, char *rfile){
 int main()
 {
      
-    fileSEND("localhost", 31337, "original.pdf", "recieved.pdf");
+    fileSEND("localhost", 31337, "4kpic.jpg", "4kpic.jpg");
     return 0;
 }
